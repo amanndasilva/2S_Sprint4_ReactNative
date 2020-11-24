dd9f27a3c233d006e4d8178936a15f4cd8be7a5d
Amanda Nascimento <amandinha.fia87@gmail.com>
2020-11-24 14:56:22 -0200

    Listagem de eventos finalizado

--- API-Nativa_Nyous/pages/Home/index.js
+++ API-Nativa_Nyous/pages/Home/index.js
@@ -1,28 +1,50 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Home = () => {
 
-    const [token, setToken] = useState('');
-
-    const getToken = async () => {
-        setToken(await AsyncStorage.getItem('@jwt'));
-    }
+    const [eventos, setEventos] = useState([]);
 
     //Se eu quero atualizar a minha lista todas as vezes que o state for atualizado, não precisa colocar no fetch toda vez o atualizar lista, podemos apenas usar o useEffect
     useEffect(()=>{
-        getToken();
+        listarEventos();
     }, [])
 
+    const listarEventos = () => {
+        fetch('http://10.0.0.106:5000/api/Account/Eventos')
+        .then(response => response.json())
+        .then(dados => {
+            setEventos(dados.data);
+            console.log(dados.data);
+        })
+        .catch(err => console.error(err));
+    }
+
+    const renderItem = (evento) => {
+        return(
+            <ItemEvento 
+                nome={evento.item.nome}
+                imagem={evento.item.urlImagem}
+                link={evento.item.link}
+            />
+        )
+    }
+
+    //Para comentar dentro do return {/* o código selecionado */}
     return(
         <View>
             <Text>Home</Text>
-            <Text>{token}</Text>
+            {/* <Text>{token}</Text> */}
+            
+            <FlatList 
+                data={eventos}
+                keyExtractor={item => item.id}
+                renderItem={renderItem}
+            />
         </View>
     )
 }
 
 export default Home;
-//Tentando subir o proj
